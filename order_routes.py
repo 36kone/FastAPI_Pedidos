17fc6853c32efb0dfb9ccb1c95bcb0cacf93480a
Caio Herrera <caioherrera36@gmail.com>
2025-06-24 18:07:17 -0300

    adicionado rota de finalizar e visualizar pedido de usuarios

--- order_routes.py
+++ order_routes.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter, Depends, HTTPException
 from models import Pedido, Usuario, ItemPedido
 from dependecies import pegar_sessao, verificar_token
-from schemas import PedidoSchema, ItemPedidoSchema
+from schemas import PedidoSchema, ItemPedidoSchema, ResponsePedidoSchema
 from sqlalchemy.orm import Session
+from typing import List
 
 order_router = APIRouter(prefix='/pedidos', tags=['pedidos'], dependencies=[Depends(verificar_token)])
 
@@ -32,7 +33,7 @@ async def cancelar_pedido(id_pedido: int, session: Session = Depends(pegar_sessa
     }
 
 @order_router.get('/listar')
-async def listar_pedidos(session: Session = Depends(pegar_sessao), usuario: Usuario = Depends(pegar_sessao)):
+async def listar_pedidos(session: Session = Depends(pegar_sessao), usuario: Usuario = Depends(verificar_token)):
     if not usuario.admin:
         raise HTTPException(status_code=400, detail='voce nao tem autorizacao para realizar essa operacao')
     else: 
@@ -59,3 +60,52 @@ async def adicionar_item(id_pedido: int, item_pedido_schema: ItemPedidoSchema,
         'item_id': id_pedido,
         'preco_pedido': pedido.preco
     }
+
+@order_router.post('/pedido/remover-item/{id_item_pedido}')
+async def remover_item(id_item_pedido: int,
+                         session: Session = Depends(pegar_sessao), usuario: Usuario = Depends(verificar_token)):
+    item_pedido = session.query(ItemPedido).filter(ItemPedido.id==id_item_pedido).first()
+    if not item_pedido:
+        raise HTTPException(status_code=400, detail='item no pedido nao existente')
+    pedido = session.query(Pedido).filter(Pedido.id==item_pedido.id).first()
+    if not pedido:
+        raise HTTPException(status_code=400, detail='pedido nao existente')
+    
+    if not usuario.admin and usuario.id != pedido.usuario:
+        raise HTTPException(status_code=401, detail='voce nao tem autorizacao para realizar essa operacao')
+    session.delete(item_pedido)
+    pedido.calcular_preco()
+    session.commit()
+    return{
+        'message': 'item removido com sucesso',
+        'quantidade_itens_pedido': len[pedido.itens],
+        'pedido': item_pedido.pedido
+    }
+
+@order_router.post('/pedidos/finalizar/{id_pedido}')
+async def finalizar_pedido(id_pedido: int, session: Session = Depends(pegar_sessao), usuario: Usuario = Depends(verificar_token)):
+    pedido = session.query(Pedido).filter(Pedido.id==id_pedido).first()
+    if not pedido: 
+        raise HTTPException(status_code=400, detail='pedido nao encontrado')
+    if not usuario.admin and usuario.id != pedido.usuario:
+        raise HTTPException(status_code=400, detail='voce nao tem autorizacao para realizar essa operacao')
+    pedido.status = 'FINALIZADO'
+    session.commit()
+    return{
+        'message': f'pedido {pedido.id} finalizado com sucesso',
+        'pedido': pedido
+    }
+
+@order_router.get('/pedido/{id_pedido}')
+async def visualizar_pedido(id_pedido: int, session: Session = Depends(pegar_sessao), usuario: Usuario = Depends(verificar_token)):
+    pedido = session.query(Pedido).filter(Pedido.id==id_pedido).first()
+    if not pedido: 
+        raise HTTPException(status_code=400, detail='pedido nao encontrado')
+    if not usuario.admin and usuario.id != pedido.usuario:
+        raise HTTPException(status_code=400, detail='voce nao tem autorizacao para realizar essa operacao')
+    return pedido
+
+@order_router.get('/listar/pedidos-usuario', response_model=List[ResponsePedidoSchema])
+async def listar_pedidos(session: Session = Depends(pegar_sessao), usuario: Usuario = Depends(verificar_token)):
+        pedidos = session.query(Pedido).filter(Pedido.usuario==usuario.id).all()
+        return pedidos
